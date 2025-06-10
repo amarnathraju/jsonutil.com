cc1b39d124502ac688b03858418b238ae70f461e
Amarnath Raju V <amarnath.raju.v@gmail.com>
2025-06-09 18:38:27 -0700

    fixing 404 errors when accessing pages directly

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: mode === 'production' ? '/jsonutil.com/' : '/',
   server: {
     host: "::",
     port: 8080,
